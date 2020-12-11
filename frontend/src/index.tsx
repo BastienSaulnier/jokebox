5a19f59ccfcfd004fdc2546fee00ed41403d9b47
BastienSaulnier <bastien.saul@gmail.com>
2020-12-11 07:07:17 +0100

    added router ad css vars + css reset

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,8 +3,22 @@ import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 import "./index.scss";
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Header from "./components/Header/index";
+import Home from "./pages/Home/index";
+
 ReactDOM.render(
-  <React.StrictMode></React.StrictMode>,
+  <React.StrictMode>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
