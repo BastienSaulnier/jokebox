5d692b004a2cdb05ea9a896ac29c3e649fc4ece3
BastienSaulnier <bastien.saul@gmail.com>
2020-12-12 10:43:37 +0100

    corrected reducer to display host and origin

--- frontend/src/redux/reducers/test.reducer.js
+++ frontend/src/redux/reducers/test.reducer.js
@@ -1,13 +1,13 @@
 import * as testConsts from "../consts/test.consts";
 
 const initState = {
-  test_key: 0,
+  test_key: "",
 };
 
 const articles = (state = initState, action) => {
   switch (action.type) {
     case testConsts.TEST_ACTION_FULFILLED:
-      return { ...state, test_key: state.test_key + 1 };
+      return { ...state, test_key: action.payload.data };
 
     default:
       return state;
