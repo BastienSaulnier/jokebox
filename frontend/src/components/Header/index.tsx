5e79341a76f45d34187ba7158fe119ace82f050b
BastienSaulnier <bastien.saul@gmail.com>
2020-12-13 17:41:09 +0100

    rectified header component

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,13 +1,22 @@
 import * as React from "react";
+import Nav from "../Nav";
 import "./index.scss";
+
+import Link from "../Link/index";
 
 export interface IHeaderProps {}
 
 export default function Header(props: IHeaderProps) {
   return (
     <div className="Header ">
-      <div className="HeaderContent center">
-        <h1>LaBwat'àBlag</h1>
+      <div className="headerContent center">
+        <h1>
+          <Link path="/" label="Logo" linkClassName="IndexButton" />
+        </h1>
+        <div className="navContainer">
+          <p>{process.env.NODE_ENV}</p>
+          <Nav />
+        </div>
       </div>
     </div>
   );
