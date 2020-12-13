2395fb5ede2fe3d727c856f21f8ff30aef148d8f
BastienSaulnier <bastien.saul@gmail.com>
2020-12-13 17:58:10 +0100

    made button test for api trigger

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,15 +1,15 @@
 import * as React from "react";
 import "./index.scss";
 
+import { testActionAPI } from "../../api/test.api";
+
 export interface IHomeProps {}
 
 export default function Nav(props: IHomeProps) {
   return (
     <div className="Home">
       Home page
-      <button onClick={() => console.log(process.env.NODE_ENV)}>
-        NODE_ENV
-      </button>
+      <button onClick={() => testActionAPI()}>NODE_ENV</button>
     </div>
   );
 }
