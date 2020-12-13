d9a537cd195ead1a7428f13a544234397a9aed80
BastienSaulnier <bastien.saul@gmail.com>
2020-12-13 18:47:56 +0100

    made testActionAPI function console.log the env var used for api link

--- frontend/src/api/test.api.js
+++ frontend/src/api/test.api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
+if (process.env.NODE_ENV !== "production") {
+  var API_URL = process.env.REACT_APP_DEV_API_URL;
+} else {
+  var API_URL = process.env.REACT_APP_PROD_API_URL;
+}
 export const testActionAPI = () => {
-  if (process.env.NODE_ENV !== "production") {
-    console.log(`made api call to ${process.env.REACT_APP_DEV_API_URL}`);
-  } else {
-    console.log(`made api call to ${process.env.REACT_APP_PROD_API_URL}`);
-  }
+  console.log(API_URL);
 };
