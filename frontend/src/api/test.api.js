57d1c7a728b2593561df14f51ee8c75407262265
BastienSaulnier <bastien.saul@gmail.com>
2020-12-13 19:12:09 +0100

    made api call depending on scripts with env vars

--- frontend/src/api/test.api.js
+++ frontend/src/api/test.api.js
@@ -1,10 +1,6 @@
 import axios from "axios";
+import { API_URL } from "./init.api";
 
-if (process.env.NODE_ENV !== "production") {
-  var API_URL = process.env.REACT_APP_DEV_API_URL;
-} else {
-  var API_URL = process.env.REACT_APP_PROD_API_URL;
-}
 export const testActionAPI = () => {
   console.log(API_URL);
 };
