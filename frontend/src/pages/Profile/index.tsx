d96a514f27eeece127b0a3b565a68e37fdcb88b9
BastienSaulnier <bastien.saul@gmail.com>
2020-12-14 17:57:18 +0100

    made profile page

---
+++ frontend/src/pages/Profile/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.scss";
+
+export interface IProfileProps {}
+
+export default function Profile(props: IProfileProps) {
+  return (
+    <div className="Profile">
+      <h2>Profile Page</h2>
+    </div>
+  );
+}
