acdf0827ed0db710515be6082c7219dea38e8d15
BastienSaulnier <bastien.saul@gmail.com>
2020-12-14 17:58:23 +0100

    reworked home page

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,15 +1,17 @@
 import * as React from "react";
 import "./index.scss";
 
-import { testActionAPI } from "../../api/test.api";
+import { API_URL } from "../../api/init.api";
 
 export interface IHomeProps {}
 
-export default function Nav(props: IHomeProps) {
+export default function Home(props: IHomeProps) {
   return (
     <div className="Home">
-      Home page
-      <button onClick={() => testActionAPI()}>NODE_ENV</button>
+      <h1>Home page</h1>
+      <p>
+        {process.env.NODE_ENV} : {API_URL}
+      </p>
     </div>
   );
 }
