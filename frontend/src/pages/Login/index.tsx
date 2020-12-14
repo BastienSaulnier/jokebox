ee12370635f60b816580b775af196789a4ce9a00
BastienSaulnier <bastien.saul@gmail.com>
2020-12-14 17:58:05 +0100

    made login page

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,8 +1,41 @@
 import * as React from "react";
 import "./index.scss";
 
+import AppButton from "../../components/AppButton";
+import AppInput from "../../components/AppInput";
+import AppForm from "../../components/AppForm";
+
 export interface ILoginProps {}
 
-export default function Nav(props: ILoginProps) {
-  return <div className="Login">Login page</div>;
+export default function Login(props: ILoginProps) {
+  const loginSubmit = (e) => {
+    e.preventDefault();
+    console.log("coucou du submit login");
+  };
+
+  return (
+    <div className="Login">
+      <h2>Login Page</h2>
+      <AppForm
+        formClassName="LoginForm"
+        formSubmitAction={(e) => loginSubmit(e)}
+      >
+        <AppInput
+          inputType="text"
+          inputName="username"
+          inputPlaceholder="username"
+        />
+        <AppInput
+          inputType="password"
+          inputName="password"
+          inputPlaceholder="password"
+        />
+        <AppButton
+          buttonClassName="loginSubmitButton"
+          buttonLabel="Login"
+          buttonType="submit"
+        />
+      </AppForm>
+    </div>
+  );
 }
