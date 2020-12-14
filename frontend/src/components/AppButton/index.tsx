12008789a0215a4059e6c47941fd5846ad1fab3e
BastienSaulnier <bastien.saul@gmail.com>
2020-12-14 12:46:55 +0100

    made button component

---
+++ frontend/src/components/AppButton/index.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import "./index.scss";
+
+export interface IAppButtonProps {
+  buttonClassName?: string;
+  buttonAction?: any;
+  buttonLabel?: string;
+  buttonType?: any;
+}
+
+export default function AppButton(props: IAppButtonProps) {
+  const { buttonClassName, buttonAction, buttonLabel, buttonType } = props;
+
+  return (
+    <button
+      className={buttonClassName}
+      type={buttonType}
+      onClick={buttonAction}
+    >
+      {buttonLabel}
+    </button>
+  );
+}
