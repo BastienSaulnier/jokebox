d3d733e0ca468f2d231abc7a5f2f315607fdf818
BastienSaulnier <bastien.saul@gmail.com>
2020-12-14 17:57:44 +0100

    made profile settongs page

---
+++ frontend/src/pages/ProfileSettings/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.scss";
+
+export interface IProfileSettingsProps {}
+
+export default function ProfileSettings(props: IProfileSettingsProps) {
+  return (
+    <div className="ProfileSettings">
+      <h2>ProfileSettings Page</h2>
+    </div>
+  );
+}
