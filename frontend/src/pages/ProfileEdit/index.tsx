56789112fa20c3f93d998797a21de6d927bb9368
BastienSaulnier <bastien.saul@gmail.com>
2020-12-14 17:57:30 +0100

    made profile edit page

---
+++ frontend/src/pages/ProfileEdit/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.scss";
+
+export interface IProfileEditProps {}
+
+export default function ProfileEdit(props: IProfileEditProps) {
+  return (
+    <div className="ProfileEdit">
+      <h2>ProfileEdit Page</h2>
+    </div>
+  );
+}
