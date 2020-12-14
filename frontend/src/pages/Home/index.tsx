766fb3f2675a127c6fbd9a48bd7e1cf15fd0f0cb
BastienSaulnier <bastien.saul@gmail.com>
2020-12-14 18:05:01 +0100

    removed useless vars display

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,17 +1,13 @@
 import * as React from "react";
 import "./index.scss";
 
-import { API_URL } from "../../api/init.api";
-
 export interface IHomeProps {}
 
 export default function Home(props: IHomeProps) {
   return (
     <div className="Home">
       <h1>Home page</h1>
-      <p>
-        {process.env.NODE_ENV} : {API_URL}
-      </p>
+      <p></p>
     </div>
   );
 }
