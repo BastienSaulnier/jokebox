fb7cdf134aed9bfa6f760b500c099771fdaa1a2c
BastienSaulnier <bastien.saul@gmail.com>
2020-12-14 18:00:32 +0100

    made user monitor for logged user

---
+++ frontend/src/components/UserMonitor/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./index.scss";
+
+import UserDropdown from "../UserDropdown";
+import AppLink from "../AppLink";
+
+export interface IUserMonitorProps {}
+
+export default function UserMonitor(props: IUserMonitorProps) {
+  return (
+    <div className="UserMonitor">
+      <AppLink linkPath="/profile" linkClassName="ProfileButton">
+        <h5 className="userName">John D.</h5>
+        <div className="userPictureContainer">
+          <img
+            className="userPicture"
+            src="https://randomuser.me/api/portraits/women/24.jpg"
+            alt="User Thumbnail"
+          />
+          <div className="userNotification"></div>
+        </div>
+      </AppLink>
+
+      <UserDropdown />
+    </div>
+  );
+}
