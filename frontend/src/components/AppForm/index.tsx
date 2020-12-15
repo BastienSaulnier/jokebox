55e417d8114f308edbc46fab39c996818d1be9d5
BastienSaulnier <bastien.saul@gmail.com>
2020-12-15 04:11:46 +0100

    reworked on form

--- frontend/src/components/AppForm/index.tsx
+++ frontend/src/components/AppForm/index.tsx
@@ -2,16 +2,21 @@ import * as React from "react";
 import "./index.scss";
 
 export interface IAppFormProps {
-  formSubmitAction?: any;
   formClassName?: string;
+  formAction?: any;
   children?: any;
 }
 
 export default function AppForm(props: IAppFormProps) {
-  const { formSubmitAction, formClassName, children } = props;
+  const { formClassName, formAction, children } = props;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await formAction();
+  };
 
   return (
-    <form className={formClassName} onSubmit={formSubmitAction}>
+    <form className={formClassName} onSubmit={(e) => handleSubmit(e)}>
       {children}
     </form>
   );
