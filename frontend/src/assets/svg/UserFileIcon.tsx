f9cfd9a9aa7595d57f33a1284f793e725ad445ec
BastienSaulnier <bastien.saul@gmail.com>
2021-01-15 08:27:44 +0100

    added images

---
+++ frontend/src/assets/svg/UserFileIcon.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+export default function UserFileIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      width="20"
+      height="20"
+      preserveAspectRatio="xMinYMin meet"
+      {...props}
+    >
+      <path d="M10.83 2H17a3 3 0 0 1 3 3v8a3 3 0 0 1-3 3H3a3 3 0 0 1-3-3V3a3 3 0 0 1 3-3h5c1.306 0 2.417.835 2.83 2zM17 4H9.415l-.471-1.334A1.001 1.001 0 0 0 8 2H3a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1z"></path>
+      <path d="M1 5h18v2H1z"></path>
+    </svg>
+  );
+}
